1213f44893cc35e594bfde1e3af25c2ce188fd15
ThinkStation-P920 <changtiwu@qq.com>
2024-06-29 11:27:53 +0800

    优化Elements代码结构

--- streamlit_gallery/components/elements/dashboard/player.py
+++ streamlit_gallery/components/elements/dashboard/player.py
@@ -8,17 +8,10 @@ class Player(Dashboard.Item):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._address = ""
-        self._serverNames = ""
-        self._clientNames = []
-        self._serverGPU = ""
-        self._clientGPU = []
-
-        if "role" not in st.session_state:
-            st.session_state["role"] = "server"
-
-        if "gpu" not in st.session_state:
-            st.session_state["gpu"] = "4090"
+        self._address, self._serverNames, self._serverGPU = "", "", ""
+        self._clientNames, self._clientGPU = [], []
+        st.session_state.setdefault("role", "server")
+        st.session_state.setdefault("gpu", "4090")
 
     def _set_address(self, event):
         sync()
@@ -26,165 +19,44 @@ class Player(Dashboard.Item):
 
     def _set_name(self, event):
         sync()
-        if hasattr(st.session_state.role, "props"):
-            role = st.session_state.role.props.value
-        else:
-            role = st.session_state.role
-        if hasattr(st.session_state.gpu, "props"):
-            gpu = st.session_state.gpu.props.value
-        else:
-            gpu = st.session_state.gpu
+        role = getattr(st.session_state.role, "props", st.session_state.role).value
+        gpu = getattr(st.session_state.gpu, "props", st.session_state.gpu).value
 
         if role == "server":
-            self._serverNames = event.target.value
-            self._serverGPU = gpu
+            self._serverNames, self._serverGPU = event.target.value, gpu
         else:
             self._clientNames.append(event.target.value)
             self._clientGPU.append(gpu)
 
     def get_content(self):
         id = 1
-        if self._serverNames == "":
-            return json.dumps(
-                [
-                    {
-                        "id": id,
-                        "address": "server not found",
-                        "name": "server not found",
-                        "gpu": "server not found",
-                    }
-                ]
-            )
-        result = [
-            {
-                "id": id,
-                "address": self._address,
-                "name": self._serverNames+"(server)",
-                "gpu": self._serverGPU,
-            }
-        ]
-        for name in self._clientNames:
-            id += 1
-            result.append(
-                {
-                    "id": id,
-                    "address": self._address,
-                    "name": name,
-                    "gpu": self._clientGPU[self._clientNames.index(name)],
-                }
-            )
+        if not self._serverNames:
+            return json.dumps([{"id": id, "address": "server not found", "name": "server not found", "gpu": "server not found"}])
+        result = [{"id": id, "address": self._address, "name": f"{self._serverNames}(server)", "gpu": self._serverGPU}]
+        result += [{"id": i+2, "address": self._address, "name": name, "gpu": self._clientGPU[i]} for i, name in enumerate(self._clientNames)]
         return json.dumps(result)
 
     def __call__(self):
-        with mui.Paper(
-            key=self._key,
-            sx={
-                "display": "flexƒ",
-                "flexDirection": "column",
-                "borderRadius": 3,
-                "overflow": "hidden",
-            },
-            elevation=1,
-        ):
-            with self.title_bar(padding="10px 15px 10px 15px", dark_switcher=False):
+        with mui.Paper(key=self._key, sx={"display": "flex", "flexDirection": "column", "borderRadius": 3, "overflow": "hidden"}, elevation=1):
+            with self.title_bar(padding="10px 15px", dark_switcher=False):
                 mui.icon.OndemandVideo()
                 mui.Typography("Role:")
+            self._render_input("Server IP", lazy(self._set_address))
+            self._render_input("Name", lazy(self._set_name))
+            self._render_select("Role", sync(None, "role"), [("Server", "server"), ("Client", "client")])
+            self._render_select("GPU", sync(None, "gpu"), [("NVIDIA GeForce RTX 4090", "4090"), ("NVIDIA GeForce RTX 3060", "3060"), ("NVIDIA TITAN RTX", "titan")])
+            self._render_button("添加设备信息")
 
-            with mui.Box(
-                sx={
-                    "padding": "10px 15px 10px 15px",
-                    "display": "flex",
-                    "alignItems": "center",
-                }
-            ):
-                mui.Typography(
-                    "Server IP",
-                    sx={"marginRight": "10px", "width": "100px", "textAlign": "right"},
-                )
-                mui.TextField(
-                    label="",
-                    variant="outlined",
-                    fullWidth=True,
-                    sx={"flexGrow": 1},
-                    onChange=lazy(self._set_address),
-                )
+    def _render_input(self, label, on_change):
+        with mui.Box(sx={"padding": "10px 15px", "display": "flex", "alignItems": "center"}):
+            mui.Typography(label, sx={"marginRight": "10px", "width": "100px", "textAlign": "right"})
+            mui.TextField(label="", variant="outlined", fullWidth=True, sx={"flexGrow": 1}, onChange=on_change)
 
-            with mui.Box(
-                sx={
-                    "padding": "10px 15px 10px 15px",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "marginTop": "10px",
-                }
-            ):
-                mui.Typography(
-                    "Name",
-                    sx={"marginRight": "10px", "width": "100px", "textAlign": "right"},
-                )
-                mui.TextField(
-                    label="",
-                    variant="outlined",
-                    fullWidth=True,
-                    sx={"flexGrow": 1},
-                    onChange=lazy(self._set_name),
-                )
+    def _render_select(self, label, on_change, options):
+        with mui.Box(sx={"padding": "10px 15px", "display": "flex", "alignItems": "center", "marginTop": "10px"}):
+            mui.Typography(label, sx={"marginRight": "10px", "width": "100px", "textAlign": "right"})
+            mui.Select(defaultValue=options[0][1], fullWidth=True, onChange=on_change, children=[mui.MenuItem(text, value=value) for text, value in options])
 
-            # Adding Dropdown (Select) Field
-            with mui.Box(
-                sx={
-                    "padding": "10px 15px 10px 15px",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "marginTop": "10px",
-                }
-            ):
-                mui.Typography(
-                    "Role",
-                    sx={"marginRight": "10px", "width": "100px", "textAlign": "right"},
-                )
-                mui.Select(
-                    defaultValue="server",
-                    fullWidth=True,
-                    children=[
-                        mui.MenuItem("Server", value="server"),
-                        mui.MenuItem("Client", value="client"),
-                    ],
-                    onChange=sync(None, "role"),
-                )
-
-            # Adding Dropdown (Select) Field
-            with mui.Box(
-                sx={
-                    "padding": "10px 15px 10px 15px",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "marginTop": "10px",
-                }
-            ):
-                mui.Typography(
-                    "GPU",
-                    sx={"marginRight": "10px", "width": "100px", "textAlign": "right"},
-                )
-                mui.Select(
-                    defaultValue="4090",
-                    fullWidth=True,
-                    children=[
-                        mui.MenuItem("NVIDIA GeForce RTX 4090", value="4090"),
-                        mui.MenuItem("NVIDIA GeForce RTX 3060", value="3060"),
-                        mui.MenuItem("NVIDIA TITAN RTX", value="titan"),
-                    ],
-                    onChange=sync(None, "gpu"),
-                )
-
-            # Adding a Button
-            with mui.Box(
-                sx={
-                    "padding": "10px 15px 10px 15px",
-                    "marginTop": "10px",
-                    "display": "flex",
-                    "justifyContent": "center",
-                }
-            ):
-                mui.Button(
-                    "添加设备信息", variant="contained", color="primary", onClick=sync()
-                )
+    def _render_button(self, text):
+        with mui.Box(sx={"padding": "10px 15px", "marginTop": "10px", "display": "flex", "justifyContent": "center"}):
+            mui.Button(text, variant="contained", color="primary", onClick=sync())
--- streamlit_gallery/components/elements/streamlit_app.py
+++ streamlit_gallery/components/elements/streamlit_app.py
@@ -1,13 +1,15 @@
+import logging
+import sys
+from contextlib import contextmanager
 from io import StringIO
 import json
 import random
 import streamlit as st
-
+from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx
 from pathlib import Path
 from streamlit import session_state as state
 from streamlit_elements import elements, sync, event
 from types import SimpleNamespace
-
 from .dashboard import Dashboard, Editor, Card, DataGrid, Radar, Pie, Player
 from streamlit_echarts import st_echarts
 
@@ -17,17 +19,11 @@ def get_echarts_options(accuracy_data):
         "title": {"text": "训练准确率"},
         "xAxis": {"type": "category", "data": [f"epoch{i}" for i in range(1, len(accuracy_data) + 1)]},
         "yAxis": {"type": "value"},
-        "series": [
-            {
-                "data": accuracy_data,
-                "type": "line",
-            }
-        ],
+        "series": [{"data": accuracy_data, "type": "line"}],
         "tooltip": {"trigger": "axis", "axisPointer": {"type": "cross"}},
-
     }
 
-def main():
+def setup_dashboard():
     if "w" not in state:
         board = Dashboard()
         w = SimpleNamespace(
@@ -48,40 +44,58 @@ def main():
     else:
         w = state.w
 
+def display_elements():
     with elements("demo"):
         event.Hotkey("ctrl+s", sync(), bindInputs=True, overrideDefault=True)
+        with state.w.dashboard(rowHeight=57):
+            state.w.player()
+            state.w.data_grid(state.w.player.get_content())
 
-        with w.dashboard(rowHeight=57):
-            # w.editor()
-            w.player()
-            # w.pie(w.editor.get_content("Pie chart"))
-            # w.card(w.editor.get_content("Card content"))
-            w.data_grid(w.player.get_content())
+@contextmanager
+def st_redirect(src, dst_placeholder, height=200):
+    output_func = lambda text: dst_placeholder.text_area("训练日志", text, height=height)
     
+    with StringIO() as buffer:
+        old_write = src.write
+
+        def new_write(b):
+            if get_script_run_ctx():
+                buffer.write(b)
+                output_func(buffer.getvalue())
+            else:
+                old_write(b)
+
+        try:
+            src.write = new_write
+            yield
+        finally:
+            src.write = old_write
+
+@contextmanager
+def st_stderr(dst_placeholder):
+    with st_redirect(sys.stderr, dst_placeholder):
+        yield
+
+def log_and_chart():
     col1, col2 = st.columns(2)
+    log_placeholder = col1.empty()  # 日志输出区域
+    chart_placeholder = col2.empty()  # 图表输出区域
     
-    log_buffer = StringIO()
-    with col1:
-        log_placeholder = st.empty()  # 日志输出区域
-    with col2:
-        chart_placeholder = st.empty()  # 图表输出区域
-    accuracy_data = []
-    for epoch in range(1, 50):  
-        accuracy = random.uniform(70, 100)
-        accuracy_data.append(accuracy)
-
-        # 更新日志
-        log_message = f"Epoch {epoch}: Train Accuracy: {accuracy:.2f}%\n"
-        log_buffer.write(log_message)
-        log_placeholder.text_area("训练日志", log_buffer.getvalue(), height=250)
-
-
-        # 更新图表，使用占位符更新同一个图表
-        with chart_placeholder:
-            st_echarts(get_echarts_options(accuracy_data))
+    with st_stderr(log_placeholder):
+        accuracy_data = []
+        for epoch in range(1, 50):
+            accuracy = random.uniform(70, 100)
+            accuracy_data.append(accuracy)
+            logging.warning(f"Epoch {epoch}: Train Accuracy: {accuracy:.2f}%")
+            with chart_placeholder:
+                st_echarts(get_echarts_options(accuracy_data))
 
     st.success("训练完成！")
 
+def main():
+    setup_dashboard()
+    display_elements()
+    log_and_chart()
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
